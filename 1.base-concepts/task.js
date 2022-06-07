1e4225bdcc98a6c652209a389c6c5116812fd6b9
pvova21 <podrezov08@rambler.ru>
2022-06-07 10:53:29 +0300

    first commit variablesNew

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,14 @@
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D = Math.pow(b, 2) - 4 * a * c;
-     if (D == 0) {
-      arr[0] = -b / (2 * a);
-      } 
-     else if (D > 0) {
-      arr[0] = (( -b + Math.sqrt(D)) / (2 * a));
-      arr[1] = (( -b - Math.sqrt(D)) / (2 * a));
-      }  
-     else (D < 0)
-  return arr; 
+   let arr = [];
+   let discriminant = Math.pow(b, 2) - 4 * a * c;
+   if (D == 0) {
+    arr[0] = -b / (2 * a);
+   } 
+   else if (D > 0) {
+    arr[0] = (( -b + Math.sqrt(discriminant)) / (2 * a));
+    arr[1] = (( -b - Math.sqrt(discriminant)) / (2 * a));
+   }  
+   return arr; 
   }
   
 
