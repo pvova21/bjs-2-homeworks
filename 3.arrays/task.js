0fc8f0867539bc565f11159ad706a5e4375d955b
pvova21 <podrezov08@rambler.ru>
2022-06-14 14:48:50 +0300

    Array commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+let result;
+if (arr1.length !== arr2.length) {
+  result = false
+} else {
+  result = arr1.every((element, i) => element === arr2[i])
 }
+return result;
+}
+
 
 function advancedFilter(arr) {
   let resultArr;
