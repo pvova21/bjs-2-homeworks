d610a77299ae93dc77d9f9d29f4e85be7b891c48
pvova21 <podrezov08@rambler.ru>
2022-06-14 14:30:14 +0300

    functions commit last

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,21 +1,20 @@
 // Задание 1
 function getArrayParams(arr) {
  let min, max, sum, avg;
-  min = Infinity;
-  max = -Infinity;
-  sum = 0;
-   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-     min = arr[i];
-     arr.push(min);
-  } else if (arr[i] > max) {
-	 max = arr[i];
-	 arr.push(max);
-	}
-	sum += arr[i];
+ min = Infinity;
+ max = -Infinity;
+ sum = 0;
+ for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < min) {
+   min = arr[i];
    }
+ if (arr[i] > max) {
+	 max = arr[i];
+	 }
+   sum += arr[i];
+  }
   avg = parseFloat((sum / arr.length).toFixed(2));
-  return {min: min, max: max, avg: Number(avg)};
+  return {min: min, max: max, avg: avg};
 }
 
 // Задание 2
