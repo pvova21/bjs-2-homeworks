fee4393b486478cb1ef1cbd772a6c8c78274233c
pvova21 <podrezov08@rambler.ru>
2022-06-20 12:19:53 +0300

    Classes commit

---
+++ 5.classes/task.js
@@ -0,0 +1,91 @@
+class PrintEditionItem {
+  constructor (name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;   
+  }
+
+  fix() {
+    this.state *= 1.5;
+  }
+
+  set state(newState) {
+    if (newState < 0) {
+      this._state = 0
+    } else if (newState > 100) {
+      this._state = 100
+    } else {
+      return this._state = newState 
+    }
+  }
+
+  get state() {
+    return this._state
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor (name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor (author, name, releaseDate, pagesCount) {
+    super (name, releaseDate, pagesCount);
+    this.type = 'book';
+    this.author = author;
+  }
+}
+
+class NovelBook extends Book {
+  constructor (author, name, releaseDate, pagesCount) {
+    super (author, name, releaseDate, pagesCount);
+    this.type = 'novel';
+  }
+}
+
+class FantasticBook extends Book {
+  constructor (author, name, releaseDate, pagesCount) {
+    super (author, name, releaseDate, pagesCount);
+    this.type = 'fantastic';
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor (author, name, releaseDate, pagesCount) {
+    super (author, name, releaseDate, pagesCount);
+    this.type = 'detective';
+  }
+}
+
+class Library {
+  constructor (name, books) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if(book.state > 30) {
+      this.books.push(book);
+    }
+    return this.books
+  }
+
+  findBookBy(type, value) {
+    for (let i = 0; this.books.length > i; i++) {
+      if (this.books[i][type] === value) return this.books[i]
+      }
+    return null
+  }
+
+  giveBookByName(bookName) {
+    for (let i = 0; this.books.length > i; i++) {
+      if (this.books[i].name === bookName) return this.books.splice([i],1);
+      }
+    return null
+  }
+}
