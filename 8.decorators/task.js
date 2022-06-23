68bf18844a2daad0ff0581bf1538935dd4adb8fb
pvova21 <podrezov08@rambler.ru>
2022-06-23 16:19:45 +0300

    Decorators commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,50 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...rest) {
+    let hash = rest.join(',');
+    let existResult = cache.filter(cacheRecord => cacheRecord.hash === hash);
+    if (existResult.length === 1) {
+        console.log('Из кэша: ' + existResult[0].value);
+        return 'Из кэша: ' + existResult[0].value;
+    }
+    else {
+      let value = func.call(this, ...rest);
+      console.log('Вычисляем: ' + value);
+      if (cache.length < 5) {   
+        cache.push({hash, value});
+      } 
+      else {
+        cache.unshift({hash, value});
+        cache.pop();
+      } 
+      return 'Вычисляем: ' + value;
+    }
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  func(...rest);
+  let flag = true;
+  return function (...rest) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (!flag) {
+        func.call(this, ...rest); 
+        flag = true;
+      }
+    }, ms);
+  };   
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+function debounceDecorator2(debounceDecoratorNew) {
+  let count = 0;
+  function wrapper(...rest) {
+    wrapper.history = count++;
+    return debounceDecoratorNew.call(this, ...rest);
+  }
+  return wrapper;
 }
